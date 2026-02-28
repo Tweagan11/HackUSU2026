9b3eb6f9e2ffcd56530e4ead879f8950ca23775c
Zach Royer <zach.royer14@gmail.com>
2026-02-28 12:04:51 -0700

    Added sergeant. added animations. teagan farted

--- code-sergeant/src/frontend/declarations.d.ts
+++ code-sergeant/src/frontend/declarations.d.ts
@@ -1,2 +1,23 @@
 /* Allow importing CSS files as side-effect modules */
 declare module '*.css';
+
+/* Allow importing image files as data-URI strings */
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.gif' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.svg' {
+  const src: string;
+  export default src;
+}
